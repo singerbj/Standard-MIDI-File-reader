01b57aa6d866dd645abf139dd6546765bb986684
Ben Singer <benjamin.singer@helpsystems.com>
2016-09-27 08:45:17 -0500

    make sure DOM is loaded before querying dom

--- dragdrop.js
+++ dragdrop.js
@@ -1,27 +1,36 @@
-var holder = document.getElementById('holder'),
-    state = document.getElementById('status');
+var holder, state;
+document.addEventListener("DOMContentLoaded", function (event) {
+  holder = document.getElementById('holder');
+  state = document.getElementById('status');
 
-if (typeof window.FileReader === 'undefined') {
-  state.className = 'fail';
-} else {
-  state.className = 'success';
-  state.innerHTML = 'File API & FileReader available';
-}
- 
-holder.ondragover = function () { this.className = 'hover'; return false; };
-holder.ondragend = function () { this.className = ''; return false; };
-holder.ondrop = function (e) {
-  this.className = '';
-  e.preventDefault();
+  if (typeof window.FileReader === undefined) {
+    state.className = 'fail';
+  } else {
+    state.className = 'success';
+    state.innerHTML = 'File API & FileReader available';
+  }
 
-  var file = e.dataTransfer.files[0],
+  holder.ondragover = function () {
+    this.className = 'hover';
+    return false;
+  };
+  holder.ondragend = function () {
+    this.className = '';
+    return false;
+  };
+  holder.ondrop = function (e) {
+    this.className = '';
+    e.preventDefault();
+
+    var file = e.dataTransfer.files[0],
       reader = new FileReader();
-  reader.onload = function (event) {
-    decodeSMF(event.target.result);
+    reader.onload = function (event) {
+      decodeSMF(event.target.result);
+    };
+    reader.onerror = function (event) {
+      alert("Error: " + reader.error);
+    };
+    reader.readAsArrayBuffer(file);
+    return false;
   };
-  reader.onerror = function (event) {
-    alert("Error: " + reader.error );
-  };
-  reader.readAsArrayBuffer(file);
-  return false;
-};
+});
